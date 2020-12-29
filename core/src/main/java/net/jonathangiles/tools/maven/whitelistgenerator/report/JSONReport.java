4164ad7513caaf07d32c976ab1479910e43dabaa
Srikanta <srnagar@microsoft.com>
2020-12-29 14:01:50 -0800

    Create report file and required dirs before writing

--- core/src/main/java/net/jonathangiles/tools/maven/whitelistgenerator/report/JSONReport.java
+++ core/src/main/java/net/jonathangiles/tools/maven/whitelistgenerator/report/JSONReport.java
@@ -3,7 +3,9 @@ package net.jonathangiles.tools.maven.whitelistgenerator.report;
 import com.google.gson.GsonBuilder;
 import net.jonathangiles.tools.maven.whitelistgenerator.model.Result;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Path;
 
 public class JSONReport implements Reporter {
@@ -14,17 +16,33 @@ public class JSONReport implements Reporter {
     }
 
     @Override
-    public void report(Result result, Path reportFile) {
-        try (FileWriter writer = new FileWriter(reportFile.toFile()))  {
+    public void report(Result result, Path reportFilePath) {
+        FileWriter writer = null;
+        try {
+            File reportFile = reportFilePath.toFile();
+            if (!reportFile.exists()) {
+                System.out.println("Creating file " + reportFile.getAbsolutePath());
+                reportFile.getParentFile().mkdirs();
+                reportFile.createNewFile();
+            }
+            writer = new FileWriter(reportFile);
             new GsonBuilder()
                     .setPrettyPrinting()
                     .excludeFieldsWithoutExposeAnnotation()
                     .create()
                     .toJson(result, writer);
 
-            System.out.println("JSON report written to " + reportFile);
+            System.out.println("JSON report written to " + reportFilePath);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
